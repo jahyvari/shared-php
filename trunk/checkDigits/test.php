0651e7a865d4ce950c7792dcfca045b0505f9109
jarkkohyvarinen1@gmail.com <jarkkohyvarinen1@gmail.com@0f89138b-16aa-8005-46e8-d1dacfa79a2e>
2014-10-10 10:42:13 +0000

    Pari bugikorjausta + lisätty TestSuite testiluokka

---
+++ trunk/checkDigits/test.php
@@ -0,0 +1,71 @@
+<?php        
+    error_reporting(E_ALL);
+    ini_set("display_errors","1");
+
+    require_once(dirname(__DIR__).
+                 DIRECTORY_SEPARATOR.
+                 "testSuite".
+                 DIRECTORY_SEPARATOR.
+                 "classTestSuite.php"
+    );
+    require_once(__DIR__.DIRECTORY_SEPARATOR."classCheckDigits.php");            
+    
+    # Tarkasta kelvollinen suomalainen BBAN    
+    TestSuite::test("digits-1",function(){
+        return CheckDigits::checkFIBBAN("159030776");
+    },true);
+    
+    # Tarkasta epäkelpo suomalainen BBAN    
+    TestSuite::test("digits-2",function(){
+        return CheckDigits::checkFIBBAN("15903072376");
+    },false);
+    
+    # Tarkasta kelvollinen suomalainen HETU
+    TestSuite::test("digits-3",function(){
+        return CheckDigits::checkFIPersonId("131052-308T");
+    },true);
+    
+    # Tarkasta epäkelo suomalainen HETU
+    TestSuite::test("digits-4",function(){
+        return CheckDigits::checkFIPersonId("131062-308T");
+    },false);
+    
+    # Tarkasta kelvollinen suomalainen Y-tunnus
+    TestSuite::test("digits-5",function(){
+        return CheckDigits::checkFIBusinessId("0737546-2");
+    },true);
+    
+    # Tarkasta epäkelo suomalainen Y-tunnus
+    TestSuite::test("digits-6",function(){
+        return CheckDigits::checkFIBusinessId("0737547-2");
+    },false);
+    
+    # Tarkasta kelvollinen IBAN
+    TestSuite::test("digits-7",function(){
+        return CheckDigits::checkIBAN("FI3715903000000776");
+    },true);
+    
+    # Tarkasta epäkelo IBAN
+    TestSuite::test("digits-8",function(){
+        return CheckDigits::checkIBAN("FI3515903000000776");
+    },false);
+                    
+    # Tarkasta kelvollinen RF viite
+    TestSuite::test("digits-9",function(){
+        return CheckDigits::checkRFReference("RF97C2H5OH");
+    },true);
+    
+    # Tarkasta epäkelo RF viite
+    TestSuite::test("digits-10",function(){
+        return CheckDigits::checkRFReference("RF97C3H5OH");
+    },false);
+    
+    # Tarkasta kelvollinen suomalainen viite
+    TestSuite::test("digits-11",function(){
+        return CheckDigits::checFIReference("6174354");
+    },true);
+    
+    # Tarkasta epäkelo suomalainen viite
+    TestSuite::test("digits-12",function(){
+        return CheckDigits::checFIReference("6174334");
+    },false);
---
+++ trunk/testSuite/classTestSuite.php
@@ -0,0 +1,214 @@
+<?php
+    class TestSuite {
+        private static $_sanityCheck = false;
+        
+        /**
+         * Funktion avulla voidaan verrata täsmääkö kaksi muuttujaa toisiinsa.
+         * 
+         * @param   mixed   $excepted   Odotettu paluuarvo
+         * @param   mixed   $result     Paluuarvo
+         * @return  bool
+         */
+        private static function _compare($excepted,$result) {
+            # Arvot täsmäävät täysin
+            if ($excepted === $result) {
+                return true;
+            }
+            
+            $floatPattern = "^[0-9]{1,}(\.[0-9]{1,})?$";
+            $intPattern = "^[0-9]{1,}$";            
+            
+            # Verrataan odotettua paluuarvoa ja paluuarvoa datatyypin konversion avulla
+            $compare = false;
+            $type = gettype($excepted);
+            switch ($type) {
+                case "boolean": # Boolean
+                    if ($excepted === (bool)$result) {
+                        $compare = true;
+                    }
+                    break;
+                
+                case "integer": # Kokonaisluku
+                    if (is_string($result) || is_numeric($result)) {
+                        if (mb_ereg_match($intPattern,$result) &&
+                            $excepted === (int)$result) {
+                            $compare = true;
+                        }
+                    } else if (is_bool($result)) {
+                        $compare = self::_compare($excepted,(int)$result);
+                    }
+                    break;
+                
+                case "double": # Liukuluku
+                    if (is_string($result) || is_numeric($result)) {
+                        if (mb_ereg_match($floatPattern,$result)) {
+                            # Liukuluvut ovat riittävän lähellä toisiaan
+                            if (abs($excepted-(double)$result) < 0.0000000001) {
+                                $compare = true;
+                            }
+                        }
+                    }
+                    break;
+                
+                case "string": # Merkkijono
+                    if (is_string($result) || is_numeric($result)) {
+                        if (mb_ereg_match($intPattern,$excepted) &&
+                                mb_ereg_match($intPattern,$result)) {
+                            # Molemmat arvot ovat kokonaislukuja merkkijonossa
+                            $compare = self::_compare((int)$excepted,(int)$result);
+                        } else if (mb_ereg_match($floatPattern,$excepted) &&
+                                mb_ereg_match($floatPattern,$result)) {
+                            # Molemmat arvot ovat liukulukuja merkkijonossa
+                            $compare = self::_compare((double)$excepted,(double)$result);
+                        }
+                    } else if (is_bool($result)) {
+                        $compare = self::_compare($excepted,(string)$result);
+                    }
+                    break;
+                
+                default: # Muu tietotyyppi
+                    # Tietotyypit ovat samoja ja täsmävät löysällä vertailulla
+                    if ($type == gettype($result) && $excepted == $result) {
+                        $compare = true;
+                    }
+            }
+                        
+            return $compare;
+        }
+        
+        /**
+         * Tarkasta _compare funktion kelvollisuus ja lopeta skriptin suoritus
+         * mikäli funktio ei toimi halutulla tavalla.
+         *
+         * @return  bool
+         */
+        private static function _sanity() {
+            try {
+                if (!self::_compare(true,true)) {
+                    throw new Exception("Sanity check true==true failed!");
+                }
+                
+                if (!self::_compare(false,false)) {
+                    throw new Exception("Sanity check false==false failed!");
+                }
+                
+                if (self::_compare(true,false)) {
+                    throw new Exception("Sanity check true==false failed!");
+                }
+                
+                if (!self::_compare(1,1)) {
+                    throw new Exception("Sanity check 1==1 failed!");
+                }
+                
+                if (!self::_compare(1,"1")) {
+                    throw new Exception("Sanity check 1=='1' failed!");
+                }
+                
+                if (!self::_compare(1,true)) {
+                    throw new Exception("Sanity check 1==true failed!");
+                }
+                
+                if (!self::_compare("1",true)) {
+                    throw new Exception("Sanity check '1'==true failed!");
+                }
+                                
+                if (self::_compare(0,true)) {
+                    throw new Exception("Sanity check 0!=true failed!");
+                }
+                                
+                if (self::_compare("0",true)) {
+                    throw new Exception("Sanity check '0'!=true failed!");
+                }
+                
+                if (!self::_compare(0.5,0.5)) {
+                    throw new Exception("Sanity check 0.5==0.5 failed!");
+                }
+                
+                if (!self::_compare(0.5,"0.5")) {
+                    throw new Exception("Sanity check 0.5==0.5 failed!");
+                }
+                
+                if (self::_compare(0.4,0.39999)) {
+                    throw new Exception("Sanity check 0.4!=0.39999 failed!");
+                }
+                
+                if (!self::_compare(array(1),array(1))) {
+                    throw new Exception("Sanity check array(1)==array(1) failed!");
+                }
+                
+                if (self::_compare(array(1),array(2))) {
+                    throw new Exception("Sanity check array(1)!=array(2) failed!");
+                }
+                
+                if (self::_compare(array(1,2),array(2,1))) {
+                    throw new Exception("Sanity check array(1,2)!=array(2,1) failed!");
+                }
+                
+                if (!self::_compare(array("b" => 2,"a" => 1),
+                    array("a" => "1","b" => 2))) {
+                    throw new Exception("Sanity check array('b'=>2,'a'=>1)==array('b'=>2,'a'=>1) failed!");
+                }
+                
+                if (!self::_compare(new TestSuite(),new TestSuite())) {
+                    throw new Exception("Sanity check TestSuite()==TestSuite() failed!");
+                }
+                
+                if (self::_compare(new TestSuite(),(object)1)) {
+                    throw new Exception("Sanity check TestSuite()!=(object)1 failed!");
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage()."\n";
+                exit(1);
+            }
+            
+            return true;
+        }
+        
+        /**
+         * Funktion avulla voidaan testata palauttaako anymyymina funktiona
+         * syötetty testitapaus odotetun paluuarvon.
+         *
+         * Funktio lopettaa skriptin suorituksen mikäli arvot eivät täsmää.
+         *
+         * @param   string      $name       Testin nimi
+         * @param   function    $function   Anonyymi funktio
+         * @param   mixed       $excepted   Odotettu paluuarvo (oletus TRUE)
+         * @return  bool
+         */
+        public static function test($name,$function,$excepted = true) {
+            # Tarkastetaan testiluokan kelvollisuus
+            if (!self::$_sanityCheck) {
+                self::_sanity();
+                self::$_sanityCheck = true;    
+            }
+                
+            try {                                
+                if (!is_callable($function)) {
+                    throw new Exception("Function is not callable!");
+                }                                
+                
+                $result = $function();
+                $compare = self::_compare($excepted,$result);
+                
+                # Palaute ei täsmää 
+                if (!$compare) {
+                    $dump1 = var_export($excepted,true);
+                    $dump2 = var_export($result,true);
+                    throw new Exception(<<<END
+Excepted value and returned value do not match!
+--------
+Excepted: $dump1
+--------
+Returned: $dump2
+--------
+END
+);
+                }
+            } catch (Exception $e) {
+                echo "Error at test suite $name: ".$e->getMessage()."\n";
+                exit(1);
+            }
+            
+            return true;
+        }
+    }
