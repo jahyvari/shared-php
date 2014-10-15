c5161817ee18c73ea2434014a80b455a0a6f329d
jarkkohyvarinen1@gmail.com <jarkkohyvarinen1@gmail.com@0f89138b-16aa-8005-46e8-d1dacfa79a2e>
2014-10-15 18:45:46 +0000

    Lisätty JS funktiot checkFIBBAN ja _checkFIBBAN

--- trunk/JS/checkDigits/jsCheckDigits.js
+++ trunk/JS/checkDigits/jsCheckDigits.js
@@ -1,22 +1,44 @@
-/**
- * Tarkastaa merkkijonon säännöllistä lauseketta vastaan.
- *
- * @param   string  val     Merkkijono
- * @param   string  pattern Säännöllinen lauseke
- * @returns bool
- */
-function _matchPattern(val,pattern) {
-    var result = false;
-    var re = new RegExp(pattern);
-    
-    if (typeof val == "string" && re.test(val)) {
-        result = true;
+var SPModulo = new function() {
+    /**
+     * Tarkastaa arvon Luhn mod 10 tarkisteen.
+     *
+     * @param   string  val     Tarkastettava arvo
+     * @returns bool
+     */
+    this.checkLuhnMod10 = function(val) {
+        var result = false;
+        
+        if (/^[0-9]{2,}$/.test(val)) {
+            val = val.toString();                      
+            
+            var multiplier = 2;
+            var sum = 0;
+            
+            for (var i = (val.length-2); i >= 0; i--) {
+                var tmp = parseInt(val.charAt(i))*multiplier--;
+                if (tmp >= 10) {
+                    tmp -= 9;
+                }
+                
+                sum += tmp;
+                
+                if (multiplier == 0) {
+                    multiplier = 2;
+                }
+            }
+            
+            if (sum > 0) {
+                var check = 10-sum%10;
+                
+                if (val.slice(-1) == check.toString().slice(-1)) {
+                    result = true;
+                }
+            }
+        }
+        
+        return result;
     }
     
-    return result;
-}
-
-var Modulo = new function() {
     /**
      * Tarkastaa arvon modulo 10 tarkisteen.
      *
@@ -26,12 +48,13 @@ var Modulo = new function() {
     this.checkModulo10 = function(val) {
         var result = false;
         
-        if (_matchPattern(val,/^[0-9]{2,}$/)) {
+        if (/^[0-9]{2,}$/.test(val)) {
+            val = val.toString();                      
+            
             var multiplier = 3;
-            var strlen = val.length;
             var sum = 0;
             
-            for (i = (strlen-2); i >= 0; i--) {
+            for (var i = (val.length-2); i >= 0; i--) {
                 sum += parseInt(val.charAt(i))*multiplier;
                 multiplier = ((multiplier == 3) ? 1 : 3);
             }
@@ -39,7 +62,7 @@ var Modulo = new function() {
             if (sum > 0) {
                 var check = 10-sum%10;
                 
-                if (val.charAt(strlen-1) == check.toString().slice(-1)) {
+                if (val.slice(-1) == check.toString().slice(-1)) {
                     result = true;
                 }
             }
@@ -49,7 +72,19 @@ var Modulo = new function() {
     }
 }
 
-var CheckDigits = new function() {
+var SPCheckDigits = new function() {
+    var _this = this;
+    
+    var _strRepeat = function(input,multiplier) {
+        var result = "";
+        
+        for (var i = 0; i < multiplier; i++) {
+            result += input.toString();
+        }
+        
+        return result;
+    }
+    
     /**
      * Tarkastaa EAN-koodin (EAN-8, EAN-13 tai EAN-14).
      *
@@ -59,8 +94,58 @@ var CheckDigits = new function() {
     this.checkEANCode = function(ean) {
         var result = false;
         
-        if (_matchPattern(ean,/^([0-9]{8}|[0-9]{13}|[0-9]{14})$/)) {
-            result = Modulo.checkModulo10(ean);
+        if (/^([0-9]{8}|[0-9]{13}|[0-9]{14})$/.test(ean)) {
+            result = SPModulo.checkModulo10(ean);
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Tarkastaa suomalaisen konekielisen tai esitysmuotoisen (viivallisen)
+     * BBAN tilinumeron.
+     *
+     * @param   string  bban    BBAN
+     * @returns bool
+     */
+    this.checkFIBBAN = function(bban) {
+        var result = false;
+        
+        if (/^[1-8]{1}[0-9]{5}(\-)?[0-9]{2,8}$/.test(bban)) {
+            result = _this._checkFIBBAN(bban.replace("-",""));
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Tarkastaa suomalaisen konekielisen BBAN tilinumeron.
+     *
+     * @param   string  bban    BBAN
+     * @returns bool
+     */
+    this._checkFIBBAN = function(bban) {
+        var result = false;
+        
+        if (/^[1-8]{1}[0-9]{7,13}$/.test(bban)) {
+            bban = bban.toString();
+            
+            var part1 = bban.slice(0,6);
+            var part2 = bban.slice(6);
+            var count = 8-part2.length;
+            
+            switch (bban.slice(0,1)) {
+                case "4":
+                case "5":
+                    part2 = part2.slice(0,1)+
+                        _strRepeat("0",count)+
+                        part2.slice(1);
+                    break;                    
+                default:
+                    part2 = _strRepeat("0",count)+part2;
+            }
+            
+            result = SPModulo.checkLuhnMod10(part1+part2);
         }
         
         return result;
