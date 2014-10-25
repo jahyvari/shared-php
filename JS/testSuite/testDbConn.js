1e916c50c24e85a40d7091bceb1b01913189c196
jarkkohyvarinen1@gmail.com <jarkkohyvarinen1@gmail.com@0f89138b-16aa-8005-46e8-d1dacfa79a2e>
2014-10-25 08:24:25 +0000

    JS: Viimeistelty testDbConn.js

--- JS/testSuite/testDbConn.js
+++ JS/testSuite/testDbConn.js
@@ -12,18 +12,95 @@ var db = require(path.dirname(__dirname)+
 var SPTestSuite = ts.SPTestSuite;
 var db = new db.SPMySQL();
 
-co(function* (){    
-    console.log(yield db.preparedQuery("SELECT * FROM test WHERE id=:id",{"id" : 79}));
-    
+co(function* (){        
     // Testaa yhteyden avaus
-    /*var open = yield db.connect();
+    var connect = yield db.connect();
     SPTestSuite.test("dbconn-1",function (){
-        return open;
+        return connect;
+    },true);
+    
+    // Testaa transaktion aloittaminen
+    var begin = yield db.begin();
+    SPTestSuite.test("dbconn-2",function (){
+        return begin;
+    },true);
+    
+    // Testaa INSERT
+    var random = Math.floor((Math.random() * 100) + 1);
+    var insertid = yield db.insert("test",{"key1" : random});
+    SPTestSuite.test("dbconn-3",function (){
+        return Boolean(insertid);
+    },true);
+    
+    // Tarkasta tallentuiko data oikeasti
+    var query = yield db.preparedQuery("SELECT * FROM test WHERE id=:id",{"id" : insertid});
+    SPTestSuite.test("dbconn-4",function (){
+        if (query) {
+            return query[0]["key1"];   
+        } else {
+            return false;
+        }        
+    },random);
+    
+    // Aloita sisäkkäinen transaktio
+    var savepoint = yield db.begin();
+    SPTestSuite.test("dbconn-5",function (){
+        return savepoint;
+    },true);
+    
+    // Muuta key1 sarakkeen arvoa
+    var newVal = random+"a";
+    var affrows = yield db.update("test",{"key1" : newVal},insertid);
+    SPTestSuite.test("dbconn-6",function (){
+        return affrows;
+    },1);
+    
+    // Tarkasta muuttuiko data oikeasti
+    var query = yield db.preparedQuery("SELECT * FROM test WHERE id=:id",{"id" : insertid});
+    SPTestSuite.test("dbconn-7",function (){
+        if (query) {
+            return query[0]["key1"];   
+        } else {
+            return false;
+        }        
+    },newVal);
+    
+    // Peru sisäkkäinen transaktio
+    var rollback = yield db.rollback();
+    SPTestSuite.test("dbconn-8",function (){
+        return rollback;
+    },true);    
+    
+    // Tarkasta menikö rollback oikeasti läpi
+    var query = yield db.preparedQuery("SELECT * FROM test WHERE id=:id",{"id" : insertid});
+    SPTestSuite.test("dbconn-9",function (){
+        if (query) {
+            return query[0]["key1"];   
+        } else {
+            return false;
+        }        
+    },random);
+    
+    // Hyväksy muutokset
+    var commit = yield db.commit();
+    SPTestSuite.test("dbconn-10",function (){
+        return commit;
+    },true);
+    
+    // Tarkasta että virheellinen SQL lause ei mene läpi
+    var query = yield db.query("SELECT * FROM test2");
+    SPTestSuite.test("dbconn-11",function (){
+        return query;   
+    },false);
+    
+    // Tarkasta että edellinen lause palautti virheilmoituksen
+    SPTestSuite.test("dbconn-12",function (){
+        return Boolean(db.error.length);   
     },true);
     
     // Testaa yhteyden sulkeminen
-    var close = yield db.disconnect();
-    SPTestSuite.test("dbconn-2",function (){
-        return close;
-    },true);*/
+    var disconnect = yield db.disconnect();
+    SPTestSuite.test("dbconn-13",function (){
+        return disconnect;
+    },true);
 })();
